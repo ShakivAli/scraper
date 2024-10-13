7dcc6aa6cd39d268f8458ef74a0356fddb50bbb9
ShakivAli <shakivaliamir123@gmail.com>
2024-10-13 14:45:44 +0530

    new commit

--- webscrap/settings.py
+++ webscrap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qa^fw#i60tawo%ly@x@9!j16yuo68%d9loo!jm)sh1+_-f+ji1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
